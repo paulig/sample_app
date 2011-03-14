fa2c75c791402d055faa25164428d0c6e2e2c0e9
Jaakko Haikola <jaakko.haikola@gmail.com>
2011-03-14 20:09:58 +0200

    muutoksia

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,8 +16,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => "#{@base_title} | Home")
       end
       
     it "should have a non-blank body" do
@@ -31,9 +35,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => "#{@base_title} | Home")
     end
   end
 
@@ -47,7 +49,7 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                         :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                         "#{@base_title} | Home")
     end
   end
 end
