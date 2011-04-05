7b3dac7d5c2de46934af03aadfd0cbaa775ec08c
Jaakko Haikola <jaakko.haikola@gmail.com>
2011-04-05 20:29:43 +0300

    Access control

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,17 +22,35 @@ module SessionsHelper
     current_user = nil # tai .self.current_user. Jos käyttää lyhyempää muotoa spec_helper =>   Rspec.configure do |config| :  controller.sign_in(user)    
   end
   
+  def current_user?
+    @user == current_user
+  end
+  
   def deny_access
+    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
   end
   
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+    
+  def clear_return_to
+    session[:return_to] = nil
+  end
+      
   private
   
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
+  def user_from_remember_token
+    User.authenticate_with_salt(*remember_token)
+  end
         
-    def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
-    end 
+  def remember_token
+    cookies.signed[:remember_token] || [nil, nil]
+  end 
 end
